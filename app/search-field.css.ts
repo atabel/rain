bc8d721ad281b95c5563148d382a5a7dc810179f
Abel Toledano <atoledano@tuenti.com>
2023-11-02 14:24:51 +0100

    improve search field

--- app/search-field.css.ts
+++ app/search-field.css.ts
@@ -12,7 +12,7 @@ export const label = style({
     position: 'absolute',
     pointerEvents: 'none',
     top: 0,
-    left: 12,
+    left: 48,
     height: '100%',
     display: 'flex',
     alignItems: 'center',
@@ -21,12 +21,24 @@ export const label = style({
 
     selectors: {
         [`${field}:focus-within &`]: {
-            transform: 'translate(-6px, -16px) scale(0.75)',
+            transform: 'translate(-6px, -12px) scale(0.75)',
         },
         [`${fieldWithValue} &`]: {
-            transform: 'translate(-6px, -16px) scale(0.75)',
+            transform: 'translate(-6px, -12px) scale(0.75)',
         },
     },
+});
+
+export const searchIcon = style({
+    position: 'absolute',
+    pointerEvents: 'none',
+    top: 0,
+    left: 0,
+    height: '100%',
+    padding: '0 12px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 });
 
 export const input = style({
@@ -39,8 +51,8 @@ export const input = style({
     background: vars.colors.backgroundContainer,
     border: `1px solid ${vars.colors.border}`,
     height: 56,
-    borderRadius: 8,
-    padding: '24px 48px 8px 12px',
+    borderRadius: 16,
+    padding: '24px 48px 8px 48px',
     width: '100%',
     boxSizing: 'border-box',
     color: vars.colors.text,
@@ -54,4 +66,28 @@ export const loadingIndicator = style({
     height: '100%',
     display: 'flex',
     alignItems: 'center',
+});
+
+export const clearButton = style({
+    position: 'absolute',
+    right: 12,
+    top: 0,
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+
+    // reset button styles
+    border: 'none',
+    margin: 0,
+    padding: 0,
+    width: 'auto',
+    overflow: 'visible',
+    background: 'transparent',
+    color: 'inherit',
+    font: 'inherit',
+    lineHeight: 'normal',
+    WebkitFontSmoothing: 'inherit',
+    WebkitAppearance: 'none',
+    appearance: 'none',
+    cursor: 'pointer',
 });
--- app/search-field.tsx
+++ app/search-field.tsx
@@ -1,8 +1,10 @@
 'use client';
-
+import * as React from 'react';
 import {usePathname, useRouter, useSearchParams} from 'next/navigation';
 import {useId, useState, useTransition} from 'react';
 import * as styles from './search-field.css';
+import {vars} from './global.css';
+import Link from 'next/link';
 
 const SearchField = () => {
     const id = useId();
@@ -11,12 +13,15 @@ const SearchField = () => {
     const searchParams = useSearchParams();
     const [isPending, startTransition] = useTransition();
     const search = searchParams.get('search') ?? '';
+    const inputRef = React.useRef<HTMLInputElement>(null);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newSearchParams = new URLSearchParams([
-            ...Array.from(searchParams).filter(([key]) => key !== 'search'),
-            ['search', e.target.value],
-        ]);
+    const searchFor = (value: string) => {
+        const newSearchParams = new URLSearchParams(searchParams);
+        if (value) {
+            newSearchParams.set('search', value);
+        } else {
+            newSearchParams.delete('search');
+        }
         startTransition(() => {
             router.replace(pathname + '?' + newSearchParams.toString());
         });
@@ -24,17 +29,25 @@ const SearchField = () => {
 
     return (
         <div className={search ? [styles.field, styles.fieldWithValue].join(' ') : styles.field}>
+            <div className={styles.searchIcon}>
+                <svg height="24" viewBox="0 -960 960 960" width="24" fill={vars.colors.textSecondary}>
+                    <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+                </svg>
+            </div>
             <label className={styles.label} htmlFor={id}>
                 Buscar
             </label>
             <input
+                ref={inputRef}
                 defaultValue={search}
                 id={id}
                 className={styles.input}
                 type="search"
-                onChange={handleChange}
+                onChange={(e) => {
+                    searchFor(e.target.value);
+                }}
             />
-            {isPending && (
+            {isPending ? (
                 <div className={styles.loadingIndicator}>
                     <svg width="24" height="24" stroke="currentColor" viewBox="0 0 24 24">
                         <style>{`.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}`}</style>
@@ -43,7 +56,23 @@ const SearchField = () => {
                         </g>
                     </svg>
                 </div>
-            )}
+            ) : search ? (
+                <button
+                    className={styles.clearButton}
+                    onClick={() => {
+                        if (!inputRef.current) {
+                            return;
+                        }
+                        inputRef.current.value = '';
+                        inputRef.current.blur();
+                        searchFor('');
+                    }}
+                >
+                    <svg height="24" viewBox="0 -960 960 960" width="24" fill={vars.colors.text}>
+                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                    </svg>
+                </button>
+            ) : null}
         </div>
     );
 };
